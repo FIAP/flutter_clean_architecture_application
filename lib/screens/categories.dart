151cb22973bbf8c6f9b158ee3dbd6a7e51c00243
Dan Vitoriano <vitoriano08@gmail.com>
2024-11-09 21:22:04 -0300

    add categories grid view, update app bar icon, add assets image and categories conts into utils

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture_application/utils/const/categories.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
@@ -7,18 +8,29 @@ class Categories extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        title: const Text('Escolha uma categoria'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              // Navigate to the search screen
-            },
-          ),
-        ],
-      ),
-      body: const Center(child: Text('Categories')),
-    ));
+            appBar: AppBar(
+              title: const Text('Escolha uma categoria'),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.bookmark),
+                  onPressed: () {
+                    // Navigate to the search screen
+                  },
+                ),
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8),
+              child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 8,
+                    mainAxisSpacing: 8,
+                    mainAxisExtent: 200,
+                  ),
+                  children: 
+                    categories.keys.map((e) => Text("Category: ${categories[e]}")).toList(),
+                  ),
+            )));
   }
 }
---
+++ lib/utils/const/categories.dart
@@ -0,0 +1,9 @@
+const String imagePath = "assets/images/";
+
+const Map<String, String> categories = {
+  'technology': 'Tecnologia',
+  'science': 'Ciência',
+  'sports': 'Esportes',
+  'health': 'Saúde',
+  'business': 'Negócios',
+};
